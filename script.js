75518ff179c3dc598e75e703e6b5ea3e88820e4d
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-10 15:45:59 -0400

    Write getComputerChoice function
    The computer can randomly choose between rock, paper, and scissors

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello world!");
+console.log(getComputerChoice());
+
+/*
+Randomly returns either "rock", "paper", or "scissors"
+*/
+function getComputerChoice()
+{
+    //come up with a random integer number either 0, 1, or 2
+    //Math.random generates a float between 0 and 1.
+    //floor rounds down
+    const rand=(Math.floor(Math.random() * 3));
+
+    if (rand===0)
+    {
+        return "rock";
+    }
+    else if (rand===1)
+    {
+        return "paper";
+    }
+    else
+    {
+        return "scissors";
+    }
+}
