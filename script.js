fd28870d3af4e52ae1dcbb654ab6838b69478d43
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-12 13:58:47 -0400

    Make rock, paper, scissor button work
    The button click event works for selecting human choice. ie When user presses the Rock button, the game plays a round with humanChoice being rock.

--- script.js
+++ script.js
@@ -40,29 +40,32 @@ function getHumanChoice()
 
 
 
-/*
-Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
-
-Create a new function named playGame.
-Move your playRound function and score variables so that they’re declared inside of the new playGame function
-
-Play 5 rounds by calling playRound 5 times.
-Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the choice functions to get new choices for each round.
-Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return values to something more useful.
-If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
-*/
-
 function playGame()
 {
     let humanScore=0;
     let computerScore=0;
 
+    //make all 3 choice buttons (rock, paper, and scissors) call handleChoice function when clicked on
+    const choiceButtons = document.querySelectorAll(".choiceBtn");
+    choiceButtons.forEach((choiceButton) => {
+        choiceButton.addEventListener("click", handleChoice);
+    });
+
+
+    function handleChoice(e)
+    {
+        //call playRound passing in the parameter of the clicked button's text (eg Rock, Paper, Scissors)
+        const playerSelection=e.target.innerHTML;
+        playRound(playerSelection);
+    } 
         
     /*
     Displays who won the round and increments the correct score variable
     */
-    function playRound(humanChoice, computerChoice)
+    function playRound(humanChoice)
     {
+        const computerChoice=getComputerChoice();
+
         const humanChoiceLower=humanChoice.toLowerCase();
         if (humanChoiceLower===computerChoice)
         {
@@ -85,25 +88,20 @@ function playGame()
     }
 
 
-    //play 5 rounds
-    /*
-    for (let i=0; i<5; i++)
+    function displayGameWinner()
     {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    */
-
-    if (humanScore==computerScore)
-    {
-        console.log("The game was a tie! You each won "+humanScore+" rounds");
-    }
-    else if (humanScore>computerScore)
-    {
-        console.log("You win! You won "+humanScore+" rounds and computer won "+computerScore+" rounds.");
-    }
-    else
-    {
-        console.log("You lose! You won "+humanScore+" rounds and computer won "+computerScore+" rounds.");
+        if (humanScore==computerScore)
+        {
+            console.log("The game was a tie! You each won "+humanScore+" rounds");
+        }
+        else if (humanScore>computerScore)
+        {
+            console.log("You win! You won "+humanScore+" rounds and computer won "+computerScore+" rounds.");
+        }
+        else
+        {
+            console.log("You lose! You won "+humanScore+" rounds and computer won "+computerScore+" rounds.");
+        }
     }
     
 
